6d980b67f5fb8edd8b07538f55160120015304f7
michelpinto <michabbe9@gmail.com>
2022-10-17 08:37:18 +0200

    fetching images using axios

--- src/app/data/dataSlice.ts
+++ src/app/data/dataSlice.ts
@@ -1,6 +1,31 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { AppThunk, AppDispatch } from '../store';
 import { Comment } from '../types';
+const axios = require('axios');
+const API_URL = 'https://picsum.photos/v2/list?page=2&limit=6';
+
+export const imageSlice = createSlice({
+  name: 'data',
+  initialState: {
+    images: [],
+  },
+  reducers: {
+    setImages: (state, action: PayloadAction<[]>) => {
+      state.images = action.payload;
+    },
+  },
+});
+
+export const { setImages } = imageSlice.actions;
+
+export const getImages = (): AppThunk => async (dispatch: AppDispatch) => {
+  try {
+    const response = await axios.get(API_URL);
+    dispatch(setImages(response.data));
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 const initialState: Comment[] = [];
 
